6f628a83939fe36ed3cddcb26f64752cd3e6e3ea
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-01-30 15:45:41 -0800

    Added a utilities.js file with all funcitons
    link and functions work as desired
    including the defaults for Mr Nobody
    and The Sign by Ace of Base

--- liri.js
+++ liri.js
@@ -1,70 +1,19 @@
 require("dotenv").config();
 
-var request = require('request');
-
-var keys = require("./keys.js");
-
-var Spotify = require('node-spotify-api');
-var Twitter = require('twitter');
-
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var utilities = require("./utilities");
 
 var operation = process.argv[2]
 var selection = process.argv[3]
-// console.log(operation);
-// console.log(selection)
 
 if (operation === "movie-this") {
-    request("https://www.omdbapi.com/?t=" + selection + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-        
-      var movieStuff = JSON.parse(body);
-      console.log("Title: " + movieStuff.Title);
-      console.log("Year of Release: " + movieStuff.Year);
-      console.log("IMDB Rating: " + movieStuff.Ratings[0].Value);
-      console.log("Rotten Tomatoes Rating: " + movieStuff.Ratings[1].Value);
-      console.log("Country of Production: " + movieStuff.Country);
-      console.log("Language: " + movieStuff.Language);
-      console.log("Plot: " + movieStuff.Plot);
-      console.log("Actors: " + movieStuff.Actors);
-    });
-   
+    utilities.movieThis(selection);
 }
 
 if (operation === "my-tweets") {
-    var params = {screen_name: 'fitwithjeremy'};
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (!error) {
-            // var myTweets = JSON.parse(tweets);
-            for (var i = 0; i < tweets.length; i++) {
-            console.log("");
-            console.log("@fitwithjeremy: " + tweets[i].text);
-            console.log("----------------------------------");
-           
-            }
-        }
-    });
-
+    utilities.myTweets();
 }
 
 if (operation === "spotify-this-song"){
-    spotify.search({ type: 'track', query: selection, limit: 1 }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-        // console.log(data);
-        console.log("Artists Name: " + data.tracks.items[0].artists[0].name); // name of artist
-        console.log("Song Name: " + data.tracks.items[0].name); // name of song
-        console.log("Spotify Preview: " + data.tracks.items[0].external_urls.spotify); // preview link
-        console.log("Album: " + data.tracks.items[0].album.name); //name of album
-       
-    //   console.log("A song by: " + data.tracks.items[0].album.artists.external_urls[0]); 
-    //   console.log("Titled: " + selection); 
-    //   console.log("Titled: " + data.tracks.items[0].album.artists.external_urls[3]); 
-
-
-      
-      });
-      
+    utilities.spotifyThis(selection);
 }
 
---
+++ utilities.js
@@ -0,0 +1,62 @@
+var request = require('request');
+var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+function movieThis(selection) {
+    if (!selection) {
+        selection = "Mr Nobody"
+    }
+
+    request("https://www.omdbapi.com/?t=" + selection + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+        
+      var movieStuff = JSON.parse(body);
+      console.log("Title: " + movieStuff.Title);
+      console.log("Year of Release: " + movieStuff.Year);
+      console.log("IMDB Rating: " + movieStuff.Ratings[0].Value);
+      console.log("Rotten Tomatoes Rating: " + movieStuff.Ratings[1].Value);
+      console.log("Country of Production: " + movieStuff.Country);
+      console.log("Language: " + movieStuff.Language);
+      console.log("Plot: " + movieStuff.Plot);
+      console.log("Actors: " + movieStuff.Actors);
+    });
+}
+
+function myTweets() {
+    var params = {screen_name: 'fitwithjeremy'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+            console.log("");
+            console.log("@fitwithjeremy: " + tweets[i].text);
+            console.log("----------------------------------");
+           
+            }
+        }
+    })
+}
+
+function spotifyThis(selection) {
+    if (!selection) {
+        selection = "The Sign Ace of Base"
+    }
+    spotify.search({ type: 'track', query: selection, limit: 1 }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        console.log("Artists Name: " + data.tracks.items[0].artists[0].name); // name of artist
+        console.log("Song Name: " + data.tracks.items[0].name); // name of song
+        console.log("Spotify Preview: " + data.tracks.items[0].external_urls.spotify); // preview link
+        console.log("Album: " + data.tracks.items[0].album.name); //name of album
+    })
+}
+
+module.exports = {
+    movieThis: movieThis,
+    myTweets: myTweets,
+    spotifyThis: spotifyThis
+}

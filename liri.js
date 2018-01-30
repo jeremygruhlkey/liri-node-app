2c50f86c290b9d9204ae4c3cd27f2979337171e0
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-01-29 19:40:21 -0800

    My-tweets funciton works
    spotify returning data that
    needs to be handled to
    display only what I want

--- liri.js
+++ liri.js
@@ -4,18 +4,21 @@ var request = require('request');
 
 var keys = require("./keys.js");
 
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 var operation = process.argv[2]
 var selection = process.argv[3]
 // console.log(operation);
 // console.log(selection)
 
-if (operation = "movie-this") {
+if (operation === "movie-this") {
     request("https://www.omdbapi.com/?t=" + selection + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
         
-      var movieStuff = JSON.parse(body)
+      var movieStuff = JSON.parse(body);
       console.log("Title: " + movieStuff.Title);
       console.log("Year of Release: " + movieStuff.Year);
       console.log("IMDB Rating: " + movieStuff.Ratings[0].Value);
@@ -26,5 +29,39 @@ if (operation = "movie-this") {
       console.log("Actors: " + movieStuff.Actors);
     });
    
+}
+
+if (operation === "my-tweets") {
+    var params = {screen_name: 'fitwithjeremy'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            // var myTweets = JSON.parse(tweets);
+            for (var i = 0; i < tweets.length; i++) {
+            console.log("");
+            console.log("@fitwithjeremy: " + tweets[i].text);
+            console.log("----------------------------------");
+           
+            }
+        }
+    });
+
+}
+
+if (operation === "spotify-this-song"){
+    spotify.search({ type: 'track', query: selection }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        console.log(data);
+        console.log(data.tracks.items[0]);
+       
+    //   console.log("A song by: " + data.tracks.items[0].album.artists.external_urls[0]); 
+    //   console.log("Titled: " + selection); 
+    //   console.log("Titled: " + data.tracks.items[0].album.artists.external_urls[3]); 
+
+
+      
+      });
+      
 }
 
